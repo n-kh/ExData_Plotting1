9ba853ad2b98f75a9c351e746cd443233f00805f
n-kh <nimit.khurana@gmail.com>
2015-02-09 15:04:48 +0530

    yolo 5

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 setwd("C:/nk2")
 file1<- read.table("./data/household_power_consumption.txt", header = TRUE,  sep=";", comment.char="%", stringsAsFactors=FALSE, na.strings="?")
 file1$Date <- as.Date(file1$Date , format = "%d/%m/%y")
-file1$global_active_power <- as.Numeric(file1$global_active_power)
-hist(as.numeric(file1$global_active_power))
+file2<-file1[file1$Date=="1/2/2007"|file1$Date == "2/2/2007",]
+hist(file2$Global_active_power)
